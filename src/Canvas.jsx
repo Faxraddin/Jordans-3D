d6e49baae1e8c1ac65591f978a29490de2e9e766
Faxraddin <reydivonay@gmail.com>
2024-02-21 23:15:59 +0400

    sections

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -2,6 +2,7 @@ import { Canvas } from "@react-three/fiber";
 import { Environment } from "@react-three/drei";
 import { OrbitControls } from "@react-three/drei";
 import { Suspense } from "react";
+import Loading from './components/Loading';
 
 import Car from "./components/Car";
 
@@ -20,7 +21,7 @@ export default function CanvasContainer () {
                 enableRotate={false}
                 enableZoom={false}
             />
-            <Suspense>
+            <Suspense fallback={<Loading/>}>
                 <Car/>
             </Suspense>
             <Environment preset="city" />
