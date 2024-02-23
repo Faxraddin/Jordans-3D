9911aeaed69034dbb98a4714144824ba426ccf8f
Faxraddin <reydivonay@gmail.com>
2024-02-23 19:23:52 +0400

    car works

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -50,6 +50,86 @@ export default function Car(props) {
   //  scene.rotation.z = sceneRotation.z
   //})
 
+  useLayoutEffect(()=>{
+    new ScrollTrigger({});
+
+    tl.to(camera.position,{
+      x: 5,
+			y: 4.0,
+			z: 2.8,
+			scrollTrigger: {
+				trigger: ".second-section",
+				start: "top bottom",
+				end: "top top",
+				scrub: true,
+				immediateRender: false,
+			},
+    })
+    .to(scene.position, {
+      x: 3.01,
+      y: 0.76,
+      z: 3.7,
+      scrollTrigger: {
+        trigger: ".second-section",
+        start: "top bottom",
+        end: "top top",
+        scrub: true,
+        immediateRender: false,
+      },
+    })
+
+    .to(scene.rotation, {
+      x: -0.53,
+      y: -3.48,
+      z: -0.21,
+      scrollTrigger: {
+        trigger: ".second-section",
+        start: "top bottom",
+        end: "top top",
+        scrub: true,
+        immediateRender: false,
+      },
+    })
+
+    //Second part
+    .to(camera.position, {
+      x: 5,
+      y: 3.8,
+      z: 2.8,
+      scrollTrigger: {
+        trigger: ".third-section",
+        start: "top bottom",
+        end: "top top",
+        scrub: true,
+        immediateRender: false,
+      },
+    })
+    .to(scene.position, {
+      x: 2.31,
+      y: 0.01,
+      z: -0.7,
+      scrollTrigger: {
+        trigger: ".third-section",
+        start: "top bottom",
+        end: "top top",
+        scrub: true,
+        immediateRender: false,
+      },
+    })
+    .to(scene.rotation, {
+      x: 0.67,
+      y: -12.9,
+      z: 0.79,
+      scrollTrigger: {
+        trigger: ".third-section",
+        start: "top bottom",
+        end: "top top",
+        scrub: true,
+        immediateRender: false,
+      },
+    });
+  },[])
+
   return (
     <group ref={car} rotation-x={[-Math.PI * 0.5]} position={[2, 1, -1.5]} castShadow scale={0.7} {...props} dispose={null}>
       <group 
